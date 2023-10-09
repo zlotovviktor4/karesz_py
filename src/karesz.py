3c470dd94f5e506c82cd9b0ca5027bb733a2f5b7
zlotovviktor4 <99753289+zlotovviktor4@users.noreply.github.com>
2023-10-09 14:57:41 +0200

    added move function in karesz

--- src/karesz.py
+++ src/karesz.py
@@ -4,7 +4,7 @@ class Karesz:
     def __init__(self,screen:Screen,labirinth:Labirinth):
         posX=0
         self.posX=0
-        self.posY=40
+        self.posY=1
         self.screen=screen
         self.screen.setKaresz(self)
         self.labirinth=labirinth
@@ -16,7 +16,11 @@ class Karesz:
     def getPicture(self):
         return self.picture
     def getPos(self):
-        return (self.posX,self.posY)
+        return [self.posX*40,self.posY*40]
+    def move(self,x:int, y:int):
+        self.posX+=x
+        self.posY+=y
+    
 
 
     
